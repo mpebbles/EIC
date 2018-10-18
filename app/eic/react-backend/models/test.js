d61f3fb74f54274d3072f390840dd8bb8ff5b0a1
geoh12 <henryx75@gmail.com>
2018-10-18 13:42:22 -0700

    added test models

--- app/eic/react-backend/models/test.js
+++ app/eic/react-backend/models/test.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var testSchema = new Schema({
-	first_name: {type:String , required: true , max 100},
-	last_name: {type:String , required: true , max 100},
+	first_name: {type:String , required: true , max: 100},
+	last_name: {type:String , required: true , max: 100},
 });
 
 module.exports = mongoose.model('test',testSchema);
