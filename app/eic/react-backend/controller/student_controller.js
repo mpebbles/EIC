bcf2e29c4ae19211500b12299e48f4259ae0f570
bdshepard <38389952+bdshepard@users.noreply.github.com>
2018-11-04 12:31:09 -0800

    added reject_pending_buddy

--- app/eic/react-backend/controller/student_controller.js
+++ app/eic/react-backend/controller/student_controller.js
@@ -96,6 +96,27 @@ exports.accept_pending_buddy = function(req, res, next) {
 	}
 }
 
+//Deletes student from this buddy's pending_student[]
+exports.reject_pending_buddy = function(req, res, next) {
+	if(!goog_token.validate_student_call(req)){
+		res.send('401 ERROR UNAUTHORISED TOKEN');
+	}
+	else{
+		googleUser.find({ eic_token : req.params.goog_token })
+		.exec(function(err,a_user){
+		if(err){return next(err)};
+			student.find({ 'contact': a_user.contact})
+     			.exec(function(err, a_student){
+         			if (err) return err;
+         			a_student.pending_buddy.deleteOne({'contact': req.params.buddy_email }
+				.exec(function(err, a_buddy){
+             			if(err) return err;
+         			})
+     			});
+		});
+	}
+}
+
 //View this student's pending buddies
 exports.get_pending_buddy = function(req, res, next) {
 	if(!goog_token.validate_student_call(req)){
