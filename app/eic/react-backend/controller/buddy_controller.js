43523714ec52b7dc023efe6b49c65b565f064a56
bdshepard <38389952+bdshepard@users.noreply.github.com>
2018-11-08 02:29:54 -0800

    api call to delete buddy/student connection

--- app/eic/react-backend/controller/buddy_controller.js
+++ app/eic/react-backend/controller/buddy_controller.js
@@ -165,6 +165,37 @@ exports.reject_pending_student = function(req, res, next) {
 	}
 }
 
+//Delete student from this buddy's student[]
+exports.reject_student = function(req, res, next) {
+	if(!goog_token.validate_buddy_call(req)){
+		res.send('401 ERROR UNAUTHORISED TOKEN');
+	}
+	else{
+		var token_to_find_in_db = JSON.stringify(req.headers.authorization).split(" ")[1];
+		token_to_find_in_db = token_to_find_in_db.substring(0,token_to_find_in_db.length - 1);
+		findEmailByToken(token_to_find_in_db, function(err,contact){
+		if(err){return next(err)};
+		buddy.findOne({'contact': contact})
+		.exec(function(err, a_buddy){
+			if (err) return err;
+			student.findOne({ 'contact': req.params.student_email })
+			.exec(function(err, a_student){
+				if(err) return err;
+				student.findOneAndUpdate(
+					{ _id: a_student._id },
+					{ $pull: {buddy: a_buddy._id}
+				}).exec();
+				buddy.findOneAndUpdate(
+					{ _id: a_buddy._id },
+					{ $pull: {student: a_student._id}
+				}).exec();
+			})
+		});
+		res.send();
+		});
+	}
+}
+
 //View this buddy's pending students
 exports.get_pending_student = function(req, res, next) {
 	if(!goog_token.validate_student_call(req)){
