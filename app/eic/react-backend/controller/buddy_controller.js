368c6daf8e16b8c238000bc9233c4ad617547b03
bdshepard <38389952+bdshepard@users.noreply.github.com>
2018-11-12 12:33:52 -0800

    reject calls added and add_pending updated

--- app/eic/react-backend/controller/buddy_controller.js
+++ app/eic/react-backend/controller/buddy_controller.js
@@ -72,29 +72,33 @@ exports.get_buddy_partial = function(req,res,next){
 	});
   }
 }
-/*
+
 //Adds student to this buddy's pending_student[]
 exports.add_pending_student = function(req, res, next) {
 	if(!goog_token.validate_buddy_call(req)){
 		res.send('401 ERROR UNAUTHORISED TOKEN');
 	}
 	else{
-		googleUser.find({ eic_token : req.params.goog_token })
-		.exec(function(err,a_user){
+		var token_to_find_in_db = JSON.stringify(req.headers.authorization).split(" ")[1];
+		token_to_find_in_db = token_to_find_in_db.substring(0,token_to_find_in_db.length - 1);
+		findEmailByToken(token_to_find_in_db, function(err,contact){
 		if(err){return next(err)};
-			buddy.find({ 'contact': a_user.contact})
-     			.exec(function(err, a_buddy){
-         			if (err) return err;
-         			student.find({ 'contact': req.params.student_email }
-				.exec(function(err, a_student){
-             			if(err) return err;
-             			a_buddy.pending_student.push(a_student);
-         			})
-     			});
+		buddy.findOne({'contact': contact})
+		.exec(function(err, a_buddy){
+			if (err) return err;
+			student.findOne({ 'contact': req.params.student_email })
+			.exec(function(err, a_student){
+				buddy.findOneAndUpdate(
+					{ _id: a_buddy.id },
+					{ $push: {pending_student: a_student._id}}
+				).exec();
+			})
+		});
+		res.send();
 		});
 	}
 }
-*/
+
 // Adds student to this buddy's student[] from pending_student[]
 // Also adds buddy to student's buddy[]
 exports.accept_pending_student = function(req, res, next) {
@@ -136,7 +140,7 @@ exports.accept_pending_student = function(req, res, next) {
 }
 
 
-//Delete student from this buddy's pending_student[]
+//Delete student from this buddy's pending_student[] and this buddy from the the specified student's pending_buddy[]
 exports.reject_pending_student = function(req, res, next) {
 	if(!goog_token.validate_buddy_call(req)){
 		res.send('401 ERROR UNAUTHORISED TOKEN');
@@ -167,7 +171,7 @@ exports.reject_pending_student = function(req, res, next) {
 	}
 }
 
-//Delete student from this buddy's student[]
+//Delete student/buddy connection
 exports.reject_student = function(req, res, next) {
 	if(!goog_token.validate_buddy_call(req)){
 		res.send('401 ERROR UNAUTHORISED TOKEN');
