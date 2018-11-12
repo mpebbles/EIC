6b77cd200624600f1301aa7ca350eb640338ed90
bdshepard <38389952+bdshepard@users.noreply.github.com>
2018-11-12 13:05:23 -0800

    reject api added and connection api updated

--- app/eic/react-backend/controller/student_controller.js
+++ app/eic/react-backend/controller/student_controller.js
@@ -70,7 +70,6 @@ exports.get_student_partial = function(req,res,next){
 }
 
 //Adds buddy to this student's pending_buddy[],
-// also adds student to buddy's pending_student[]
 exports.add_pending_buddy = function(req, res, next) {
 	if(!goog_token.validate_student_call(req)){
 		res.send('401 ERROR UNAUTHORISED TOKEN');
@@ -78,46 +77,57 @@ exports.add_pending_buddy = function(req, res, next) {
 	else{
 		var token_to_find_in_db = JSON.stringify(req.headers.authorization).split(" ")[1];
 		token_to_find_in_db = token_to_find_in_db.substring(0,token_to_find_in_db.length - 1);
-  	findEmailByToken(token_to_find_in_db, function(err, contact) {
+  		findEmailByToken(token_to_find_in_db, function(err, contact) {
 			if(err){return next(err)};
 			student.findOne({'contact': contact})
-     	.exec(function(err, a_student){
-        if (err) return err;
-        buddy.findOne({ 'contact': req.params.buddy_email })
+     			.exec(function(err, a_student){
+        			if (err) return err;
+        			buddy.findOne({ 'contact': req.params.buddy_email })
 				.exec(function(err, a_buddy){
-      		if(err) return err;
+      					if(err) return err;
 					student.findOneAndUpdate(
-   					{ _id: a_student._id },
-   					{ $push: {pending_buddy: a_buddy._id}
+   						{ _id: a_student._id },
+   						{ $push: {pending_buddy: a_buddy._id}
 					}).exec();
-					buddy.findOneAndUpdate(
-						{ _id: a_buddy._id },
-						{ $push: {pending_student: a_student._id}
-					}).exec();
-      	})
-      });
+      				})
+     			 });
 		});
 	}
 }
 
-/*
-// NOT UPDATED since sprint 2
 //Adds student to this buddy's student[] from pending_student[]
 exports.accept_pending_buddy = function(req, res, next) {
 	if(!goog_token.validate_student_call(req)){
 		res.send('401 ERROR UNAUTHORISED TOKEN');
 	}
 	else{
-		googleUser.find({ eic_token : req.params.goog_token })
-		.exec(function(err,a_user){
+		var token_to_find_in_db = JSON.stringify(req.headers.authorization).split(" ")[1];
+		token_to_find_in_db = token_to_find_in_db.substring(0,token_to_find_in_db.length - 1);
+		findEmailByToken(token_to_find_in_db, function(err, contact) {
 		if(err){return next(err)};
-			student.find({ 'contact': a_user.contact})
+			student.findOne({ 'contact': contact})
      			.exec(function(err, a_student){
-         			if (err) return err;
-         			a_student.pending_buddy.find({'contact': req.params.buddy_email }
+        			if (err) return err;
+				buddy.find({"_id": {$in: a_student.pending_buddy}})
 				.exec(function(err, a_buddy){
-             			if(err) return err;
-             			a_student.buddy.push(a_buddy);
+        				if(err) return err;
+          				student.findOneAndUpdate(
+   						{ _id: a_student._id },
+   						{ $pull: {pending_buddy: a_buddy._id}
+					}).exec();
+					student.findOneAndUpdate(
+   						{ _id: a_student._id },
+   						{ $push: {buddy: a_buddy._id}
+					}).exec();
+//**this next call shouldn't be necessary**
+					buddy.findOneAndUpdate(
+   						{ _id: a_buddy._id },
+   						{ $pull: {pending_student: a_student._id}
+					}).exec();
+					buddy.findOneAndUpdate(
+   						{ _id: a_buddy._id },
+   						{ $push: {student: a_student._id}
+					}).exec();
          			})
      			});
 		});
@@ -144,6 +154,7 @@ exports.reject_pending_buddy = function(req, res, next) {
 					{ _id: a_student._id },
 					{ $pull: {pending_buddy: a_buddy._id}
 				}).exec();
+//**this next call shouldn't be necessary**
 				buddy.findOneAndUpdate(
 					{ _id: a_buddy._id },
 					{ $pull: {pending_student: a_student._id}
@@ -155,7 +166,7 @@ exports.reject_pending_buddy = function(req, res, next) {
 	}
 }
 
-//Delete buddy from this student's buddy[]
+//Delete buddy/student connection
 exports.reject_student = function(req, res, next) {
 	if(!goog_token.validate_buddy_call(req)){
 		res.send('401 ERROR UNAUTHORISED TOKEN');
@@ -186,29 +197,29 @@ exports.reject_student = function(req, res, next) {
 	}
 }
 
-// NOT UPDATED since sprint 2
 //View this student's pending buddies
 exports.get_pending_buddy = function(req, res, next) {
 	if(!goog_token.validate_student_call(req)){
 		res.send('401 ERROR UNAUTHORISED TOKEN');
 	}
 	else{
-		googleUser.find({ eic_token : req.params.goog_token })
-		.exec(function(err,a_user){
+		var token_to_find_in_db = JSON.stringify(req.headers.authorization).split(" ")[1];
+		token_to_find_in_db = token_to_find_in_db.substring(0,token_to_find_in_db.length - 1);
+		findEmailByToken(token_to_find_in_db, function(err, contact) {
 		if(err){return next(err)};
-			student.find({ 'contact': a_user.contact})
+			student.findOne({ 'contact': contact})
      			.exec(function(err, a_student){
-         			if (err) return err;
-         			a_student.pending_buddy.find()
+        			if (err) return err;
+				buddy.find({"_id": {$in: a_student.pending_buddy}})
 				.exec(function(err, a_buddy){
-             			if(err) return err;
-             			res.json({a_buddy});
+        				if(err) return err;
+          				res.json([{a_buddy}]);
          			})
      			});
 		});
 	}
 }
-*/
+
 //View this student's accepted buddies
 exports.get_buddy = function(req, res, next) {
 	if(!goog_token.validate_student_call(req)){
@@ -220,14 +231,14 @@ exports.get_buddy = function(req, res, next) {
 		findEmailByToken(token_to_find_in_db, function(err, contact) {
 		if(err){return next(err)};
 			student.findOne({ 'contact': contact})
-     	.exec(function(err, a_student){
-        if (err) return err;
+     			.exec(function(err, a_student){
+        			if (err) return err;
 				buddy.find({"_id": {$in: a_student.buddy}})
 				.exec(function(err, a_buddy){
-        	if(err) return err;
-          res.json([{a_buddy}]);
-         	})
-     	});
+        				if(err) return err;
+          				res.json([{a_buddy}]);
+         			})
+     			});
 		});
 	}
 }
