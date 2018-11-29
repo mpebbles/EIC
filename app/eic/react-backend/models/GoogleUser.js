bc6a4cd4eb0ee3f73e8beacb987c2761b6d47f99
Mitchell Pebbles <mpebbles@ucsc.edu>
2018-11-29 13:53:56 -0800

    Changed line to .contact

--- app/eic/react-backend/models/GoogleUser.js
+++ app/eic/react-backend/models/GoogleUser.js
@@ -83,7 +83,7 @@ googleUserSchema.statics.findEmailByToken = function(token_str, next_func) {
       if (user == null) {
         console.log(token_str);
       } else {
-        next_func(err, (JSON.parse(JSON.stringify(user))).email);
+        next_func(err, user.contact);
       }
     });
 };
