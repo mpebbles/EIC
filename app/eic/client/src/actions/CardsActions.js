a922f7f875caab8587bc2cc58820ec53590508c1
Mitchell Pebbles <mpebbles@ucsc.edu>
2018-12-03 20:46:42 -0800

    fixed alerts, fixed delete

--- app/eic/client/src/actions/CardsActions.js
+++ app/eic/client/src/actions/CardsActions.js
@@ -32,41 +32,37 @@ export function loadCardsData() {
 }
 
 export function deleteConnection(email) {
+  let userTypeService = new UserTypeService();
   const token = localStorage.getItem("id_token");
+  const userType = userTypeService.getUserType();
   try {
-    axios({
-      method: "get",
-      url: config.BASE_URL + "/api/get_user_type/",
-      headers: { Authorization: `Bearer ${token}` }
-    }).then(res => {
-      if (res.data === "Student") {
-        axios({
-          method: "get",
-          url: config.BASE_URL + "/api/reject_buddy/" + email,
-          headers: { Authorization: `Bearer ${token}` }
-        }).then(res_1 => {
-          if (
-            window.confirm("Are you sure you want to delete this connection?")
-          ) {
-            //alert("Connection deleted!");
-            dispatcher.dispatch({ type: "REMOVE_CARD", cardEmail: email });
-          }
-        });
-      } else if (res.data === "Buddy") {
-        axios({
-          method: "get",
-          url: config.BASE_URL + "/api/reject_student/" + email,
-          headers: { Authorization: `Bearer ${token}` }
-        }).then(res_2 => {
-          if (
-            window.confirm("Are you sure you want to delete this connection?")
-          ) {
-            //alert("Connection deleted!");
-            dispatcher.dispatch({ type: "REMOVE_CARD", cardEmail: email });
-          }
-        });
-      }
-    });
+    if (userType === "Student") {
+      axios({
+        method: "get",
+        url: config.BASE_URL + "/api/reject_buddy/" + email,
+        headers: { Authorization: `Bearer ${token}` }
+      }).then(res_1 => {
+        if (
+          window.confirm("Are you sure you want to delete this connection?")
+        ) {
+          //alert("Connection deleted!");
+          dispatcher.dispatch({ type: "REMOVE_CARD", cardEmail: email });
+        }
+      });
+    } else if (userType === "Buddy") {
+      axios({
+        method: "get",
+        url: config.BASE_URL + "/api/reject_student/" + email,
+        headers: { Authorization: `Bearer ${token}` }
+      }).then(res_2 => {
+        if (
+          window.confirm("Are you sure you want to delete this connection?")
+        ) {
+          //alert("Connection deleted!");
+          dispatcher.dispatch({ type: "REMOVE_CARD", cardEmail: email });
+        }
+      });
+    }
   } catch (err) {
     //console.log(err);
   }
